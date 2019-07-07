38b51a45d47973097ade7b2a42f868545e6325b0
nikolouzos <nickrnikolouzos@gmail.com>
2019-07-07 22:06:00 +0300

    Initial Commit

---
+++ swiftui-notes/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftui-notes
+//
+//  Created by Nikolaos Rafail Nikolouzos on 07/07/2019.
+//  Copyright © 2019 Nikolaos Rafail Nikolouzos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
