28218671da1a95e7116fbbe82d312280a12fba94
Nikolaos Rafail Nikolouzos <nickrnikolouzos@gmail.com>
2019-08-22 01:17:30 +0300

    Finished the phone login layout, refactored some code

--- swiftui-notes/Views/LaunchView.swift
+++ swiftui-notes/Views/LaunchView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct LaunchView: View {
     
@@ -33,10 +34,18 @@ struct LaunchViewPreviews: PreviewProvider {
 private struct TitleView: View {
     var body: some View {
         return VStack {
-            Text("Welcome to the Notes App!")
-                .font(.title)
-                Text("We aim to help you be your most productive self.")
-                    .font(.subheadline)
+            HStack {
+                Text("Welcome to the ")
+                    .font(.title)
+                Text("Notes App")
+                    .font(.title)
+                    .color(.orange)
+                    .bold()
+                Text("!")
+                    .font(.title)
+            }
+            Text("We aim to help you be your most productive self!")
+                .font(.subheadline)
             }
             .padding()
     }
@@ -52,34 +61,74 @@ private struct LoginView: View {
     
     var body: some View {
         return VStack {
-            if loading {
-                // TODO: Add the loading view
-            } else {
-                VStack {
-                    Text("Ready to login?")
-                    
-                    TextField($phone)
-                        .cornerRadius(20)
-                        .padding()
-                    
-                    Button(
-                        action: {
-                            // Login the user using Firebase Auth phone auth
-                    },
-                        label: {
-                            Text("Login")
-                                .font(.body)
-                    })
-                    }
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20,
-                                         style: .circular)
-                            .foregroundColor(.secondary)
-                    )
-                    .edgesIgnoringSafeArea(.all)
+            Text("Let's get started!")
+                .color(.white)
+                .font(.headline)
+                .padding(.bottom)
+            
+            HStack {
+                Text("PHONE")
+                    .font(.subheadline)
+                    .color(.white)
+                    .padding(.bottom, 0)
+                    .padding(.leading, 4)
+                Spacer()
             }
+            
+            TextField($phone, placeholder:
+                Text("Enter your phone")
+                    .color(.orange)
+                )
+                .lineLimit(1)
+                .textContentType(.telephoneNumber)
+                .padding(.all)
+                .background(Color.white, cornerRadius: 5)
+            
+            Button(
+                action: { self.verifyPhone() },
+                label: {
+                    // Change the text of the button depending on the state of the view
+                    Text("Login")
+                        .font(.body)
+                })
+                .padding(.horizontal, 28)
+                .padding(.vertical, 8)
+                .background(Color.white, cornerRadius: 5)
+            }
+            .padding(.all)
+            .foregroundColor(.orange)
+            .background(
+                RoundedRectangle(cornerRadius: 20,
+                style: .circular)
+                    .foregroundColor(.orange)
+                .edgesIgnoringSafeArea(.bottom)
+            )
+    }
+    
+    /// Validates the user's phone. If the validation is successful, a code will be sent to the
+    /// user via SMS to authenticate them
+    func verifyPhone() {
+        // Validate the user's phone using regex
+        if self.phone.range(
+            of: #"^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$"#,
+            options: .regularExpression
+            ) != nil {
+            log.info("The user's phone is \(self.phone)")
+            
+            // Show the loader
+            self.loading = true
+            
+            // Login the user using Firebase's phone auth
+            PhoneAuthProvider.provider()
+                .verifyPhoneNumber(self.phone, uiDelegate: nil,
+                                   completion: { (verificationId, error) in
+                
+                if let error = error {
+                    log.error("Firebase Auth: Error \(error.localizedDescription)")
+                } else {
+                    
+                }
+            })
         }
     }
 }
